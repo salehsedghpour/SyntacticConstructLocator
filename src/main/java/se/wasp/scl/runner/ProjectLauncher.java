bf7c1c0e93f7d16f5a095c59988c3c65c9fa8847
Mohammad Reza Saleh <mr.salehsedghpour@yahoo.com>
2021-10-25 13:35:21 +0200

    adding launcher

---
+++ src/main/java/se/wasp/scl/runner/ProjectLauncher.java
@@ -0,0 +1,29 @@
+package se.wasp.scl.runner;
+
+import se.wasp.scl.util.SyntacticEnum;
+import spoon.Launcher;
+import spoon.reflect.CtModel;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+public class ProjectLauncher {
+
+    private Launcher launcher = new Launcher();
+    private SyntacticEnum syntacticConstruct;
+    private CtModel model;
+
+    private final static Logger LOGGER = Logger.getLogger(ProjectLauncher.class.getName());
+
+    ProjectLauncher(String path, SyntacticEnum syntacticConstruct) {
+        LOGGER.info(String.format("Processing %s", path));
+        this.syntacticConstruct = syntacticConstruct;
+        launcher.getEnvironment().setCommentEnabled(true);
+        launcher.addInputResource(path);
+        launcher.buildModel();
+        model = launcher.getModel();
+    }
+    public List<String> processModel() {
+        return null;
+    }
+}
